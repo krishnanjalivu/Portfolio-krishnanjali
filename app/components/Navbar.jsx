c9e0d4c773964693c8f8d1e277a5d80fd4e06fc1
krishnanjalivu <vukrishnanjali@gmail.com>
2023-10-27 19:38:43 +0530

    commit fourth

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link
           href={"/"}
-          className="text-2xl md:text-4xl text-white font-normal font-sans"
+          className="text-2xl md:text-2xl text-white font-light font-sans logo"
         >
           Krishnanjali
         </Link>
