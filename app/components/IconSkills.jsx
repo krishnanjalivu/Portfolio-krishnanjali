370501934f53e01cfa2be13bbe7119f6a12d6279
krishnanjalivu <vukrishnanjali@gmail.com>
2023-10-27 23:24:20 +0530

    commit fifth

--- app/components/IconSkills.jsx
+++ app/components/IconSkills.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const IconSkills = () => {
   return (
     <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-      <div className="sm:border-[#33353F] sm:border rounded-md py-8 px-[40px] flex gap-4 sm:flex-row items-center justify-between">
+      <div className="sm:border-[#33353F] sm:border rounded-md py-8 mr-9 px-[30px] flex gap-4 sm:flex-row items-center justify-between">
        <Image src="/images/html-5.png" width={40} height={40} className='animate-pulse' />
        <Image src="/images/css.png" width={40} height={40} className='animate-pulse' />
        <Image src="/images/js.png" width={40} height={40} className='animate-pulse' />
