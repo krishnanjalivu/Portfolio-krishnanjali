04402f11cdc9a7c09765de3ffdf67c7a8563c658
krishnanjalivu <vukrishnanjali@gmail.com>
2023-10-27 21:57:20 +0530

    commit fifth

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,16 +1,17 @@
-//import { EmailTemplate } from '../../../components/EmailTemplate';
-// import { NextResponse } from 'next/server';
-// import { Resend } from 'resend';
+// import { EmailTemplate } from '../../../components/EmailTemplate';
+// import { NextResponse } from "next/server";
+// import { Resend } from "resend";
 
 // const resend = new Resend(process.env.RESEND_API_KEY);
-// const fromemail=process.env.FROM_EMAIL
+// const fromEmail = process.env.FROM_EMAIL;
+
 // export async function POST(req, res) {
-//     const { email, subject, message } = await req.json();
-//     console.log(email, subject, message);
+//   const { email, subject, message } = await req.json();
+//   console.log(email, subject, message);
 //   try {
 //     const data = await resend.emails.send({
-//       from: fromemail,
-//       to: [fromemail,email],
+//       from: fromEmail,
+//       to: [fromEmail, email],
 //       subject: subject,
 //       react: (
 //         <>
@@ -21,7 +22,6 @@
 //         </>
 //       ),
 //     });
-
 //     return NextResponse.json(data);
 //   } catch (error) {
 //     return NextResponse.json({ error });
--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -7,35 +7,35 @@ const EmailSection = () => {
   const [emailSubmitted, setEmailSubmitted] = useState(false);
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    const data = {
-      email: e.target.email.value,
-      subject: e.target.subject.value,
-      message: e.target.message.value,
-    };
-    const JSONdata = JSON.stringify(data);
-    const endpoint = "/api/send";
+//     e.preventDefault();
+//     const data = {
+//       email: e.target.email.value,
+//       subject: e.target.subject.value,
+//       message: e.target.message.value,
+//     };
+//     const JSONdata = JSON.stringify(data);
+//     const endpoint = "/api/send";
 
-    // Form the request for sending data to the server.
-    const options = {
-      // The method is POST because we are sending data.
-      method: "POST",
-      // Tell the server we're sending JSON.
-      headers: {
-        "Content-Type": "application/json",
-      },
-      // Body of the request is the JSON data we created above.
-      body: JSONdata,
-    };
+//     // Form the request for sending data to the server.
+//     const options = {
+//       // The method is POST because we are sending data.
+//       method: "POST",
+//       // Tell the server we're sending JSON.
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       // Body of the request is the JSON data we created above.
+//       body: JSONdata,
+//     };
 
-    const response = await fetch(endpoint, options);
-    const resData = await response.json();
+//     const response = await fetch(endpoint, options);
+//     const resData = await response.json();
 
-    if (response.status === 200) {
-      console.log("Message sent.");
-      setEmailSubmitted(true);
-    }
-  };
+//     if (response.status === 200) {
+//       console.log("Message sent.");
+//       setEmailSubmitted(true);
+//     }
+   };
 
   return (
     <section
@@ -54,11 +54,11 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com">
+          <Link href="https://github.com/krishnanjalivu">
             <Image src="/images/github.png" className="fill-current bg-white rounded-full"  alt="Github Icon" width={50}
               height={50} />
           </Link>
-          <Link href="linkedin.com">
+          <Link href="https://www.linkedin.com/in/krishnanjali-0645591b7/">
             <Image src="/images/linkedin.png" className="fill-current bg-white rounded-full" alt="Linkedin Icon" width={50}
               height={50}  />
           </Link>
